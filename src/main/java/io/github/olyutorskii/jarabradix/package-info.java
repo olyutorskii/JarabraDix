e460790028feafb5e35d8d8a7106bc6e611374e6
olyutorskii <olyutorskii@gmail.com>
2017-04-20 19:36:16 +0900

    add package

---
+++ src/main/java/io/github/olyutorskii/jarabradix/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * License : The MIT License
+ * Copyright(c) 2017 olyutorskii
+ */
+
+/**
+ * JarabraDix is a integer value to Arabic decimal sequence converter.
+ */
+
+package io.github.olyutorskii.jarabradix;
+
+/* EOF */
